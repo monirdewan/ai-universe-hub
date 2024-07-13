5f48ca9a879ff524e23dcc0375c760d8189b1104
Monir <hossinmonir681@gmail.com>
2024-07-13 09:12:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div>
+     <button className="btn btn-active btn-neutral">Neutral</button>
+     <button className="btn btn-active btn-primary">Primary</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          
+          "primary": "#00b89b",
+                    
+          "secondary": "#fe8f00",
+                    
+          "accent": "#0083ff",
+                    
+          "neutral": "#1e1213",
+                    
+          "base-100": "#1b273a",
+                    
+          "info": "#00b8ff",
+                    
+          "success": "#51ff0c",
+                    
+          "warning": "#c63c00",
+                    
+          "error": "#ff807f",
+          },
+        },
+      ],
+    },
+  plugins: [
+    daisyui,
+  ],
+}
